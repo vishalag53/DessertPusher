beb41693fbcf26571660f078e0b2b04d336650d7
vishalag53 <vishal.agrawal1207@gmail.com>
2023-05-24 08:39:13 +0530

    Add

--- app/src/main/java/com/example/dessertpusher/MainActivity.kt
+++ app/src/main/java/com/example/dessertpusher/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.dessertpusher
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.databinding.DataBindingUtil
+import com.example.dessertpusher.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        Log.i("MainActivity","onCreate Called")
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main)
     }
 }
